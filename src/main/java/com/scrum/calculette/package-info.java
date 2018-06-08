09c62c9cc63fdebf59d2a7bca4673f1bbdd7fba1
tsupio <t.supiot@iia-laval.fr>
2018-06-08 09:57:16 +0200

    end project preparation

---
+++ src/main/java/com/scrum/calculette/package-info.java
@@ -0,0 +1,10 @@
+/*
+ * Copyright (c) Ida Willy Theo 2018
+ * All rights reserved
+ */
+
+/**
+ * Package cours méthodologie Agile/Sctum - Master 2 - 2018
+ * TP Calculette.
+ */
+package com.scrum.calculette;
--- src/test/java/com/scrum/calculette/ApplicationTest.java
+++ src/test/java/com/scrum/calculette/ApplicationTest.java
@@ -1,4 +1,24 @@
+/*
+ * Copyright (c) Ida Willy Theo 2018
+ * All rights reserved
+ */
+
 package com.scrum.calculette;
 
+import org.junit.After;
+import org.junit.Before;
+
 public class ApplicationTest {
+
+    @Before
+    // S'exécute AVANT chaque test
+    public void setup(){
+        System.out.println("Before test");
+    }
+
+    @After
+    // S'exécute APRES chaque test
+    public void teardown(){
+        System.out.println("After test");
+    }
 }
